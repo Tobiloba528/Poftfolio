401398f40d626813133024d515a28e3cc9b6336a
Tobiloba <omitomo.a@gotruck.io>
2022-04-27 10:25:56 +0100

    New edited cv

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -64,7 +64,7 @@ const NavBar = ({ isOpen, setIsOpen }) => {
             <li>
               <Button>
                 <a
-                  href="https://drive.google.com/file/d/1namdBN8V0L-exjJp9IG7rO7u7fZg0hqg/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1xzB2aam1lR8OTtgbkGEIHqLr1xwZK1iR/view?usp=sharing"
                   target="_blank"
                 >
                   Resume
@@ -145,7 +145,7 @@ const NavBar = ({ isOpen, setIsOpen }) => {
           <li>
             <Button>
               <a
-                href="https://drive.google.com/file/d/1namdBN8V0L-exjJp9IG7rO7u7fZg0hqg/view?usp=sharing"
+                href="https://drive.google.com/file/d/1xzB2aam1lR8OTtgbkGEIHqLr1xwZK1iR/view?usp=sharing"
                 target="_blank"
               >
                 Resume
